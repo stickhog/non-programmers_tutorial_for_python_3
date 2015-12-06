33e5123fbb09dac0381502776314b8194254350d
stickhog <stickhog69@icloud.com>
2015-12-06 11:30:34 -0800

    started dictionaries_example

---
+++ dictionaries_example.py
@@ -0,0 +1,17 @@
+def print_menu():
+    print('1. Print Phone Numbers')
+    print('2. Add a Phone Number')
+    print('3. Remove a Phone Number')
+    print('4. Lookup a Phone Number')
+    print('5. Quit')
+    print()
+
+numbers = {}
+menu_choice = 0
+print_menu()
+while menu_choice != 5:
+    menu_choice = int(input("Type in a number (1-5): "))
+    if menu_choice == 1:
+        print("Telephone Numbers:")
+        for x in numbers.keys():
+            print("Name: ", )
